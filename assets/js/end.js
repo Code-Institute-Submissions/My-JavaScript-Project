d66317c4f5d6d3d820bb56ffdf90566e36076521
Philip Jambrisak <jambrisak@gmail.com>
2022-10-06 17:18:21 +0200

    add local storage for score

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,11 +1,12 @@
 //Constants
 const username = document.getElementById('username');
-
+const saveScoreBtn = document.getElementById('saveScoreBtn');
 //Event Listener
 username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
 
-})
 //Save high score function
 saveHighScore = (e) => {
     e.preventDefault();
-}
+};

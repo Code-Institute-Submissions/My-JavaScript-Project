bc3470335baf670d5853cb872a79b148fa5d1fdf
Philip Jambrisak <jambrisak@gmail.com>
2022-10-06 18:04:13 +0200

    update highscore.js

---
+++ assets/js/highscores.js
@@ -0,0 +1,10 @@
+//constants
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+//mapping
+highScoresList.innerHTML = highScores
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");

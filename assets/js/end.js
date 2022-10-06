5937177e5736d7e4019c5d869d31289552f265ee
Philip Jambrisak <jambrisak@gmail.com>
2022-10-06 17:37:27 +0200

    added storage and sorting to score

--- assets/js/end.js
+++ assets/js/end.js
@@ -15,6 +15,17 @@ username.addEventListener('keyup', () => {
 //Save high score function
 saveHighScore = (e) => {
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
 
 //local storage

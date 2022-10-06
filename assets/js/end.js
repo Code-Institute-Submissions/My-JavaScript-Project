031df4b5d1ed9a82d73d882869e40015da7c667c
Philip Jambrisak <jambrisak@gmail.com>
2022-10-06 17:47:28 +0200

    update end.html and end.js files

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,18 +1,17 @@
+//Constants
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
 const MAX_HIGH_SCORES = 5;
 
+//Event Listener
 finalScore.innerText = mostRecentScore;
-
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
-
+//Save high score
 saveHighScore = (e) => {
     e.preventDefault();
 

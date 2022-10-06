96489467c713da760d7bdd7a6ee2492f3b1f9910
Philip Jambrisak <jambrisak@gmail.com>
2022-10-06 17:15:20 +0200

    add event + const end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,3 +1,11 @@
+//Constants
+const username = document.getElementById('username');
+
+//Event Listener
+username.addEventListener('keyup', () => {
+
+})
+//Save high score function
 saveHighScore = (e) => {
-    
+    e.preventDefault();
 }

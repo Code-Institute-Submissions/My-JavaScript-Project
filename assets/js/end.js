197fa2a9456077124b5c655c0fcc01f41afb00fd
Philip Jambrisak <jambrisak@gmail.com>
2022-10-08 18:49:29 +0200

    fixed filepaths for end.js + end.html + highscores.html

--- assets/js/end.js
+++ assets/js/end.js
@@ -23,5 +23,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('../My-JavaScript-Project/highscores.html');
+    window.location.assign('/');
 };

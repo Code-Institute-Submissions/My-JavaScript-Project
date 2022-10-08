1e07bb96be839f94264023ac9b0b359d5cc7eb05
Philip Jambrisak <jambrisak@gmail.com>
2022-10-08 16:34:28 +0200

    add a fetch for question

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,10 @@ const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById('progressBarFull');
 //Temporary questions
 let questions = [];
+
+fetch("questions.json").then(res => {
+    return res.json();
+});
 //Start Game function
 startGame = () => {
     questionCounter = 0;
